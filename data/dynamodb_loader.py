3699881faf14257dad088166cbff0fdc40a52861
Heiby <whl@ualberta.ca>
2025-10-05 20:36:25 -0600

    loaded data

---
+++ data/dynamodb_loader.py
@@ -0,0 +1,20 @@
+import boto3
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+table_name = os.getenv("DB")
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(table_name)
+
+with open('colors.json') as file:
+    data = json.load(file)
+
+for language, info in data.items():
+    item = {
+        'language': language,
+        'colour': info.get('color'),
+        'url': info.get('url')
+    }
+    table.put_item(Item=item)
